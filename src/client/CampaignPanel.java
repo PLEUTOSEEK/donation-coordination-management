41719dcb798b1b500fdc45685c9f245d353d1560
PLEUTOSEEK <xuanvanquish@gmil.com>
2022-03-10 00:47:03 +0800

    adt

--- src/client/CampaignPanel.java
+++ src/client/CampaignPanel.java
@@ -5,6 +5,8 @@
  */
 package client;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Tee Zhuo Xuan
@@ -12,7 +14,47 @@ package client;
 public class CampaignPanel implements Panel {
 
     public void controlPanel() {
+        Scanner input = new Scanner(System.in);
 
+    }
+
+    @Override
+    public String menu() {
+        StringBuilder menu = new StringBuilder();
+
+        menu.append("1. Ad\n");
+        menu.append("2. \n");
+        menu.append("3. \n");
+        menu.append("4. \n");
+        menu.append("5. \n");
+        menu.append("6. \n");
+
+        return menu.toString();
+    }
+
+    @Override
+    public void add() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void search() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void exit() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
