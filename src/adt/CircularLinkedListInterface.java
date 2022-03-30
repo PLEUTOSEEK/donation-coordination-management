6312f36752548cb9b9c16e769fac895f0365f6a1
jaejae11 <looijt-wm19@student.tarc.edu.my>
2022-03-30 18:41:34 +0800

    comment

--- src/adt/CircularLinkedListInterface.java
+++ src/adt/CircularLinkedListInterface.java
@@ -12,36 +12,24 @@ package adt;
 public interface CircularLinkedListInterface<T> {
 
     boolean addLastNode(T element);
-
     boolean addFirstNode(T element);
-
     boolean addAtAnyNode(T element, int position);
 
-    int countNodes();
-
-    boolean isEmpty();
-
     Object getFirstNode();
-
     Object getLastNode();
-
     Object getAnyNode(int position);
 
     boolean removeFirstNode();
-
     boolean removeLastNode();
-
     boolean removeAnyNode(int position);
 
     boolean replaceFirstNode(T element);
-
     boolean replaceLastNode(T element);
-
     boolean replaceAnyNode(T element, int position);
 
+    int countNodes();
+    boolean isEmpty();    
     T[] toArray(T[] array);
-
     boolean contains(T element);
-
     int indexOf(T element);
 }
